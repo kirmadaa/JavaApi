c07397cd071a45ff13d5549049cbddf420612368
eb110 <wfigura@op.pl>
2021-12-15 16:00:02 +0000

    swagger configuration

--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -5,6 +5,8 @@ import com.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/user")
 public class UserController {
     @Autowired
     UserService userService;
